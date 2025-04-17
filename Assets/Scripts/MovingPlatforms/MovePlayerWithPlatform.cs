a6fb7c7a3de9340c8b828c23b18aa6172453c023
1033864-Mark <1033864@student.sae.edu.au>
2025-04-17 14:09:55 +1000

    debug log

--- Assets/Scripts/MovingPlatforms/MovePlayerWithPlatform.cs
+++ Assets/Scripts/MovingPlatforms/MovePlayerWithPlatform.cs
@@ -1,69 +1,74 @@
 using StarterAssets;
 using UnityEngine;
-
-public class MovePlayerWithPlatform : MonoBehaviour
+namespace TheFurtiveFour.MovingPlatforms
 {
-    [SerializeField] private Transform currentPlatform;
-    private Vector3 lastPlatformPosition;
-    [SerializeField] private MovingPlatform movingPlatform;
-    private ThirdPersonController thirdPersonController;
-    private Animator animator;
-    private void Start()
+    public class MovePlayerWithPlatform : MonoBehaviour
     {
-        thirdPersonController = GetComponent<ThirdPersonController>();
-        animator = GetComponent<Animator>();
-    }
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.collider.CompareTag("MovingPlatform"))
+        [SerializeField] private Transform currentPlatform;
+        private Vector3 lastPlatformPosition;
+        [SerializeField] private MovingPlatform movingPlatform;
+        private ThirdPersonController thirdPersonController;
+        private Animator animator;
+        // for comments write WHAT it is doing and WHY did you choose that type/function
+        private void Start()
         {
-            if (currentPlatform != hit.collider.transform)
+            thirdPersonController = GetComponent<ThirdPersonController>();
+            animator = GetComponent<Animator>();
+        }
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            if (hit.collider.CompareTag("MovingPlatform"))
             {
-                currentPlatform = hit.collider.transform;
-                movingPlatform = currentPlatform.GetComponent<MovingPlatform>();
-                lastPlatformPosition = currentPlatform.position;
-            }
-        }
-        else
-        {
-            currentPlatform = null;
-        }
-    }
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            currentPlatform = null;
-            thirdPersonController.enabled = true;
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (currentPlatform != null && movingPlatform != null)
-        {
-            if (movingPlatform.isMoving)
-            {
-                thirdPersonController.enabled = false;
-                animator.SetFloat("Speed", 0f);
-                animator.SetFloat("MotionSpeed", 0f);
-
-                if (Input.GetKeyUp(KeyCode.Space))
+                if (currentPlatform != hit.collider.transform)
                 {
-                    currentPlatform = null;
-                    thirdPersonController.enabled = true;
+                    currentPlatform = hit.collider.transform;
+                    movingPlatform = currentPlatform.GetComponent<MovingPlatform>();
+                    lastPlatformPosition = currentPlatform.position;
                 }
             }
-            else if (!movingPlatform.isMoving)
+            else
             {
+                currentPlatform = null;
+            }
+        }
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                currentPlatform = null;
                 thirdPersonController.enabled = true;
+                Debug.Log("Jumped Off Platform");
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (currentPlatform != null && movingPlatform != null)
+            {
+                if (movingPlatform.isMoving)
+                {
+                    thirdPersonController.enabled = false;
+                    animator.SetFloat("Speed", 0f);
+                    animator.SetFloat("MotionSpeed", 0f);
+
+                    if (Input.GetKeyUp(KeyCode.Space))
+                    {
+                        currentPlatform = null;
+                        thirdPersonController.enabled = true;
+                    }
+                }
+                else if (!movingPlatform.isMoving)
+                {
+                    thirdPersonController.enabled = true;
+                }
+
+                transform.position += movingPlatform.deltaMovement;
             }
 
-            transform.position += movingPlatform.deltaMovement;
+
         }
 
 
     }
-
 
 }
--- Assets/Scripts/MovingPlatforms/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatforms/MovingPlatform.cs
@@ -1,52 +1,55 @@
 using UnityEngine;
 using System.Collections;
-
-public class MovingPlatform : MonoBehaviour
+namespace TheFurtiveFour.MovingPlatforms
 {
-    public Vector3 pointA;
-    public Vector3 pointB;
-    public float speed;
-    public float waitTime;
-    public bool isMoving { get; private set; }
-    [SerializeField] private bool checkIsMoving;
-
-    private Vector3 target;
-    private Vector3 lastPosition;
-
-    public Vector3 deltaMovement { get; private set; }
-
-    void Start()
+    public class MovingPlatform : MonoBehaviour
     {
-        isMoving = false;
-        speed = 2f;
-        waitTime = 5f;
-        target = pointB;
-        lastPosition = transform.position;
-        StartCoroutine(MovePlatform());
-    }
+        public Vector3 pointA;
+        public Vector3 pointB;
+        public float speed;
+        public float waitTime;
+        public bool isMoving { get; private set; }
+        [SerializeField] private bool checkIsMoving;
 
-    void Update()
-    {
-        deltaMovement = transform.position - lastPosition;
-        lastPosition = transform.position;
-        checkIsMoving = isMoving;
-    }
+        private Vector3 target;
+        private Vector3 lastPosition;
 
-    private IEnumerator MovePlatform()
-    {
-        while (true)
+        public Vector3 deltaMovement { get; private set; }
+
+        void Start()
         {
-            isMoving = true;
-
-            while (Vector3.Distance(transform.position, target) > 0.01f)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-                yield return null;
-            }
-
             isMoving = false;
-            yield return new WaitForSeconds(waitTime);
-            target = target == pointA ? pointB : pointA;
+            speed = 2f;
+            waitTime = 5f;
+            target = pointB;
+            lastPosition = transform.position;
+            StartCoroutine(MovePlatform());
+        }
+
+        void Update()
+        {
+            deltaMovement = transform.position - lastPosition;
+            lastPosition = transform.position;
+            checkIsMoving = isMoving;
+        }
+
+        private IEnumerator MovePlatform()
+        {
+            while (true)
+            {
+                isMoving = true;
+
+                while (Vector3.Distance(transform.position, target) > 0.01f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+                    yield return null;
+                }
+
+                isMoving = false;
+                yield return new WaitForSeconds(waitTime);
+                target = target == pointA ? pointB : pointA;
+            }
         }
     }
+
 }
